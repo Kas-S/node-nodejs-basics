d1ffd9dfcdf82777838deeb16ebc6762316fb070
Kas S <sotimkul@gmail.com>
2024-10-06 13:39:17 +0500

    create.js ready

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import { writeFile, access } from 'node:fs/promises';
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await access('files/fresh.txt');
+    throw new Error('FS operation failed');
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      await writeFile('files/fresh.txt', 'I am fresh and young');
+    } else {
+      throw err;
+    }
+  }
 };
 
 await create();
