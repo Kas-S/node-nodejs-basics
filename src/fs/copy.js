18cb654d76939a738e875bc90dc816efad80e96d
Kas S <sotimkul@gmail.com>
2024-10-07 00:38:24 +0500

    copy.js done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,32 @@
+import { mkdir, readdir, copyFile } from 'node:fs/promises';
+import { cp } from 'node:fs';
+
+const FS_FAILED = new Error('FS operation failed');
+
+const dfs = async (source, destination) => {
+  await mkdir(destination, { recursive: true });
+  const entries = await readdir(source, { withFileTypes: true });
+  for (const entry of entries) {
+    const SP = `${source}/${entry.name}`;
+    const DP = `${destination}/${entry.name}`;
+    if (entry.isDirectory()) {
+      await dfs(SP, DP);
+    } else {
+      await copyFile(SP, DP);
+    }
+  }
+};
+
 const copy = async () => {
-    // Write your code here 
+  await readdir('files_copy')
+    .then(() => {
+      throw FS_FAILED;
+    })
+    .catch(() => {});
+  await readdir('files').catch(() => {
+    throw FS_FAILED;
+  });
+  await dfs('files', 'files_copy');
 };
 
 await copy();
