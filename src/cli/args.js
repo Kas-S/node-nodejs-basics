8dd0594cd132c6ef16e22a0a1ba33235b4972a1a
Kas S <sotimkul@gmail.com>
2024-10-07 23:31:00 +0500

    args.js is ready

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  let res = '';
+  for (let i = 0; i < args.length; i += 2) {
+    res += `${args[i].replace(/-/g, '')} is ${args[i + 1]}, `;
+  }
+  console.log(res.slice(0, -2));
 };
 
 parseArgs();
