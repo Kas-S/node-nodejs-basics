c1355bbe5d04cbbe8f4555740858ebbdb1e5b220
Kas S <sotimkul@gmail.com>
2024-10-07 22:55:19 +0500

    env.js is ready

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  const env = process.env;
+  let result = '';
+  Object.keys(env)
+    .filter((key) => key.startsWith('RSS_'))
+    .forEach((key) => {
+      result += `${key}=${env[key]}; `;
+    });
+  console.log(result.slice(0, -2));
 };
 
 parseEnv();
