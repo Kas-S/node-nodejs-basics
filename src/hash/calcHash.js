92c8e7e9e990b08b1a44c3b3a84f34ab859ce9f7
Kas S <sotimkul@gmail.com>
2024-10-08 00:42:18 +0500

    hash is ready

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,13 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+  const data = await readFile('files/fileToCalculateHashFor.txt', {
+    encoding: 'utf8'
+  });
+  hash.update(data);
+  console.log(hash.digest('hex'));
 };
 
 await calculateHash();
