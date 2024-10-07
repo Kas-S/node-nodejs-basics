2d741900d69793846891b29345f7a69697843ce3
Kas S <sotimkul@gmail.com>
2024-10-07 19:23:30 +0500

    rename.js is ready

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,15 @@
+import { rename as rn, access } from 'node:fs';
+
+const FS_FAILED = new Error('FS operation failed');
+
 const rename = async () => {
-    // Write your code here 
+  access('files/properFilename.md', (err) => {
+    if (!err) throw FS_FAILED;
+  });
+  access('files/wrongFilename.txt', (err) => {
+    if (err) throw FS_FAILED;
+  });
+  rn('files/wrongFilename.txt', 'files/properFilename.md', () => {});
 };
 
 await rename();
